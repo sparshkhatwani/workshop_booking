6ab67590fca08dba47f9e8d4d3071a3c33eb7cb8
Sparsh Khatwani <sparsh.khatwani@gmail.com>
2026-04-05 22:03:44 +0530

    update cms urls with path and re_path routing

--- cms/urls.py
+++ cms/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import url
+from django.urls import path, re_path
+from . import views
+# from django.conf.urls import url
 
 from cms import views
 
 app_name = "cms"
 
 urlpatterns = [
-    url('^$', views.home, name='home'),
-    url('^(?P<permalink>.+)$', views.home, name='home')
+    # url('^$', views.home, name='home'),
+    # url('^(?P<permalink>.+)$', views.home, name='home')
+    path('', views.home, name='home'),
+    re_path(r'^(?P<permalink>.+)$', views.home, name='home'),
 ]
