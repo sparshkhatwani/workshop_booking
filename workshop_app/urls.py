680d9da79d0fadd2904dcfa9005a7d5071138d40
Sparsh Khatwani <sparsh.khatwani@gmail.com>
2026-04-05 22:38:43 +0530

    updated url patterns

--- workshop_app/urls.py
+++ workshop_app/urls.py
@@ -13,42 +13,91 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import path, re_path
+from . import views
+# from django.conf.urls import url
 from workshop_app import views
 
 app_name = "workshop_app"
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^register/$', views.user_register, name="register"),
-    url(r'^activate_user/(?P<key>.+)$', views.activate_user),
-    url(r'^activate_user/$', views.activate_user),
-    url(r'^login/$', views.user_login, name="login"),
-    url(r'^logout/$', views.user_logout, name="logout"),
-    url(r'^status$', views.workshop_status_coordinator,
-        name='workshop_status_coordinator'),
-    url(r'^dashboard$', views.workshop_status_instructor,
-        name='workshop_status_instructor'),
-    url(r'^accept_workshop/(?P<workshop_id>\d+)', views.accept_workshop,
-        name='accept_workshop'),
-    url(r'^change_workshop_date/(?P<workshop_id>\d+)$',
-        views.change_workshop_date, name='change_workshop_date'),
-    url(r'^details/(?P<workshop_id>\d+)$', views.workshop_details,
-        name='workshop_details'),
-    url(r'^type_details/(?P<workshop_type_id>\d+)$',
-        views.workshop_type_details, name='workshop_type_details'),
-    url(r'^type_tnc/(?P<workshop_type_id>\d+)$',
-        views.workshop_type_tnc, name='workshop_type_tnc'),
-    url(r'^propose/$', views.propose_workshop,
-        name='propose_workshop'),
-    url(r'^add_workshop_type$', views.add_workshop_type,
-        name='add_workshop_type'),
-    url(r'^delete_attachment_file/(?P<file_id>\d+)$',
-        views.delete_attachment_file, name='delete_attachment_file'),
-    url(r'^types/$', views.workshop_type_list,
-        name='workshop_type_list'),
-    url(r'^view_profile/$', views.view_own_profile,
-        name='view_own_profile'),
-    url(r'^view_profile/(?P<user_id>\d+)$', views.view_profile,
-        name='view_profile'),
+    # url(r'^$', views.index, name='index'),
+    # url(r'^register/$', views.user_register, name="register"),
+    # url(r'^activate_user/(?P<key>.+)$', views.activate_user),
+    # url(r'^activate_user/$', views.activate_user),
+    # url(r'^login/$', views.user_login, name="login"),
+    # url(r'^logout/$', views.user_logout, name="logout"),
+    # url(r'^status$', views.workshop_status_coordinator,
+    #     name='workshop_status_coordinator'),
+    # url(r'^dashboard$', views.workshop_status_instructor,
+    #     name='workshop_status_instructor'),
+    # url(r'^accept_workshop/(?P<workshop_id>\d+)', views.accept_workshop,
+    #     name='accept_workshop'),
+    # url(r'^change_workshop_date/(?P<workshop_id>\d+)$',
+    #     views.change_workshop_date, name='change_workshop_date'),
+    # url(r'^details/(?P<workshop_id>\d+)$', views.workshop_details,
+    #     name='workshop_details'),
+    # url(r'^type_details/(?P<workshop_type_id>\d+)$',
+    #     views.workshop_type_details, name='workshop_type_details'),
+    # url(r'^type_tnc/(?P<workshop_type_id>\d+)$',
+    #     views.workshop_type_tnc, name='workshop_type_tnc'),
+    # url(r'^propose/$', views.propose_workshop,
+    #     name='propose_workshop'),
+    # url(r'^add_workshop_type$', views.add_workshop_type,
+    #     name='add_workshop_type'),
+    # url(r'^delete_attachment_file/(?P<file_id>\d+)$',
+    #     views.delete_attachment_file, name='delete_attachment_file'),
+    # url(r'^types/$', views.workshop_type_list,
+    #     name='workshop_type_list'),
+    # url(r'^view_profile/$', views.view_own_profile,
+    #     name='view_own_profile'),
+    # url(r'^view_profile/(?P<user_id>\d+)$', views.view_profile,
+    #     name='view_profile'),
+    path('', views.index, name='index'),
+    path('register/', views.user_register, name="register"),
+    
+    re_path(r'^activate_user/(?P<key>.+)$', views.activate_user),
+    path('activate_user/', views.activate_user),
+
+    path('login/', views.user_login, name="login"),
+    path('logout/', views.user_logout, name="logout"),
+
+    path('status', views.workshop_status_coordinator,
+         name='workshop_status_coordinator'),
+
+    path('dashboard', views.workshop_status_instructor,
+         name='workshop_status_instructor'),
+
+    re_path(r'^accept_workshop/(?P<workshop_id>\d+)', views.accept_workshop,
+            name='accept_workshop'),
+
+    re_path(r'^change_workshop_date/(?P<workshop_id>\d+)$',
+            views.change_workshop_date, name='change_workshop_date'),
+
+    re_path(r'^details/(?P<workshop_id>\d+)$', views.workshop_details,
+            name='workshop_details'),
+
+    re_path(r'^type_details/(?P<workshop_type_id>\d+)$',
+            views.workshop_type_details, name='workshop_type_details'),
+
+    re_path(r'^type_tnc/(?P<workshop_type_id>\d+)$',
+            views.workshop_type_tnc, name='workshop_type_tnc'),
+
+    path('propose/', views.propose_workshop,
+         name='propose_workshop'),
+
+    path('add_workshop_type', views.add_workshop_type,
+         name='add_workshop_type'),
+
+    re_path(r'^delete_attachment_file/(?P<file_id>\d+)$',
+            views.delete_attachment_file, name='delete_attachment_file'),
+
+    path('types/', views.workshop_type_list,
+         name='workshop_type_list'),
+
+    path('view_profile/', views.view_own_profile,
+         name='view_own_profile'),
+
+    re_path(r'^view_profile/(?P<user_id>\d+)$', views.view_profile,
+            name='view_profile'),
 ]
--- workshop_portal/urls.py
+++ workshop_portal/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import path, include
+from . import views
+# from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from workshop_portal import views
@@ -21,12 +23,22 @@ from django.conf import settings
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
-    url(r'^workshop/', include('workshop_app.urls')),
-    url(r'^reset/', include('django.contrib.auth.urls')),
-    url(r'^page/', include('cms.urls')),
-    url(r'^statistics/', include('statistics_app.urls')),
+    # url(r'^admin/', admin.site.urls),
+    # url(r'^$', views.index),
+    # url(r'^workshop/', include('workshop_app.urls')),
+    # url(r'^reset/', include('django.contrib.auth.urls')),
+    # url(r'^page/', include('cms.urls')),
+    # url(r'^statistics/', include('statistics_app.urls')),
+    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('workshop/', include('workshop_app.urls')),
+    path('reset/', include('django.contrib.auth.urls')),
+    path('page/', include('cms.urls')),
+    path('statistics/', include('statistics_app.urls')),
+
+    # React frontend API
+    path('api/', include('workshop_app.api_urls')),
+    path('api/statistics/', include('statistics_app.api_urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
